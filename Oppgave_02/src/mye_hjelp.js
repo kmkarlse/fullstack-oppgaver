77635f72ab9f4e5d9b4b20c54d171ff443783716
kmkarlse <77330597+kmkarlse@users.noreply.github.com>
2022-09-08 17:11:56 +0200

    fix

--- Oppgave_02/src/mye_hjelp.js
+++ Oppgave_02/src/mye_hjelp.js
@@ -1,5 +1,5 @@
 import './styles.css';
-import { users } from './util';
+import { users } from './util.js';
 
 // TODO: Hent HTML #id med getElementById
 const searchInput = document.getElementById('name');
@@ -17,33 +17,46 @@ const createTableUI = (users) => {
 
 const handleSearch = () => {
   // TODO: Hent ut verdien fra søke input feltet
-  const searchName = getElementById('name').value;
+  const searchName = searchInput.value;
   if (searchName) {
     // Bruk .find for å finne navnet til den brukeren som matcher søkeordet
-    const searchResult = getElementById('');
+    const searchResult = users.find(u => u.name.includes(searchName) );
     if (searchResult) {
       // TODO: Oppdatere grensesnittet med createTableUI og resultatet av søket
+      createTableUI(searchResult);
+
     } else {
       // TODO: Oppdatere grensesnittet med userUl.innerHTML og en passende tekst når vi ikke finner noe
+      userUl.innerHTML = "ingen ting ble funnet"
+
     }
   } else {
     // TODO: Hvis ingen søkeord vis default liste med brukere via createTableUI
+    createTableUI(users);
+
   }
 };
 
 const handleFilter = () => {
   // TODO: Hent ut verdien fra filter input feltet
-  const filterValue = '';
+  const filterValue = Number(filterInput.value);
   if (filterValue && Number(filterValue)) {
     // TODO: Bruk .filter for å hente ut de brukeren  som har en alder høyere en filterverdien
-    const filterResult = '';
+    const filterResult = users.filter(a => a.age > filterValue);
+
     if (filterResult && filterResult.length > 0) {
       // TODO: Oppdatere grensesnittet med createTableUI og resultatet av filteret
+      createTableUI(filterResult);
+
     } else {
       // TODO: Oppdatere grensesnittet med userUl.innerHTML og en passende tekst når vi ikke finner noe
+      userUl.innerHTML = 'fant ingen alder';
+
     }
   } else {
     // TODO: Hvis ingen filter eller filteret ikke er et tall vis default liste med brukere via createTableUI
+    createTableUI(users);
+
   }
 };
 
